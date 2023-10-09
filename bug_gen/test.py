cdb5a03bbd49ecdf41858701dee6f9a87cfb5584
MajikalExplosions <110285691+jliu7350@users.noreply.github.com>
2023-10-09 16:52:43 -0700

    Quick refactor and fixes

--- bug_gen/test.py
+++ bug_gen/test.py
@@ -21,6 +21,7 @@ public class Test {
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0) System.out.print("Fizz");
             if (i % 5 == 0) System.out.print("Buzz");
+            if (i % 3 != 0 && i % 5 != 0) System.out.print(i);
             System.out.println();
         }
     }
