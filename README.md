3d8a67f53646164932d88c8182a5303299882b3c
MajikalExplosions <110285691+jliu7350@users.noreply.github.com>
2023-10-09 17:11:27 -0700

    Add CLI args, and update readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # CSCI499FinalProject
+
+To call bug_gen from CLI:
+`python bug_gen.py -i <Input File Path> -o <Output File Path> -n <# of bugs to introduce, default 5>`
+
+To call bug_gen from code:
+`bug_gen.add_bugs(code, num_bugs)`
+Where code is a string of java code and num_bugs is the number of bugs to introduce.
+Returns the `code` string, with `num_bugs` introduced in it.
--- bug_gen/bug_gen.py
+++ bug_gen/bug_gen.py
@@ -1,32 +1,74 @@
 """
-Introduces bugs to provided Java code
+Introduces bugs to Java code in a file, provided through kwargs
 """
-import bugs, test, random
+import bugs, random, os
 
-def main():
-    # Composite bugs
-    for e in range(10):
-        applied = []
-        for i in range(5):
-            bt = random.randint(0, 5)
-            if bt == 0:
-                applied.append(bugs.MissingCharacterBug())
-            elif bt == 1:
-                applied.append(bugs.IncorrectOperationBug())
-            elif bt == 2:
-                applied.append(bugs.ArrayIndexBug())
-            elif bt == 3:
-                applied.append(bugs.IncorrectValueBug())
-            elif bt == 4:
-                applied.append(bugs.IncorrectConditionBug())
-        
-        code = test.CODE_1
-        for bug in applied:
-            code = bug.effect(code)
-            if (not bug.valid):
-                print(bug.name)
-        
-        print(code)
+def add_bugs(file_data, num_bugs):
+    bug_types = [bugs.ArrayIndexBug, bugs.IncorrectConditionBug, bugs.IncorrectOperationBug, bugs.IncorrectValueBug, bugs.MissingCharacterBug]
 
-if __name__ == '__main__':
-    main()
+    for i in range(num_bugs):
+        # Get a random bug
+        bug = random.choice(bug_types)
+        # Create and apply bug
+        file_data = bug().effect(file_data)
+
+    return file_data
+
+
+def bug_gen(**kwargs):
+    """
+    Introduces bugs to Java code in a file, provided through kwargs
+    """
+    # Get the file path from kwargs
+    file_path = kwargs.get("in", "")
+    # Get the number of bugs to introduce from kwargs
+    num_bugs = kwargs.get("num", 5)
+    # Get output path
+    output_path = kwargs.get("out", "")
+
+    if len(file_path) == 0:
+        raise ValueError("Input file path not provided")
+    if len(output_path) == 0:
+        raise ValueError("Output file path not provided")
+    if num_bugs < 0:
+        raise ValueError("Number of bugs must be greater than 0")
+
+    # Add .java to file paths if not already there
+    if not ".java" in file_path.lower()[-5:]:
+        file_path += ".java"
+    if not ".java" in output_path.lower()[-5:]:
+        output_path += ".java"
+    
+    print(f"Input file path: {file_path}")
+    print(f"Output file path: {output_path}")
+
+    
+    # Check to make sure paths exist
+    if not os.path.exists(file_path):
+        raise ValueError("Input file path does not exist")
+
+    # Open and read file
+    with open(file_path, "r") as file:
+        file_data = file.read()
+    
+    # Add bugs
+    file_data = add_bugs(file_data, num_bugs)
+    
+    # Write to output file
+    with open(output_path, "w") as file:
+        file.write(file_data)
+
+if __name__ == "__main__":
+    import argparse
+
+    # Create parser
+    parser = argparse.ArgumentParser(description="Introduces bugs to Java code in a file, provided through kwargs")
+    # Add arguments
+    parser.add_argument("-i", "--in", type=str, help="The input file path")
+    parser.add_argument("-o", "--out", type=str, help="The output file path")
+    parser.add_argument("-n", "--num", type=int, help="The number of bugs to introduce")
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Run bug_gen
+    bug_gen(**vars(args))
