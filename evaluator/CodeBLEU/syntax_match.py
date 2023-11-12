8736b9015cf929230810916f4600a273fb3bfb47
MajikalExplosions <jliu7350@usc.edu>
2023-11-12 15:49:46 -0800

    Rename parser to parser2 to let it compile

--- evaluator/CodeBLEU/syntax_match.py
+++ evaluator/CodeBLEU/syntax_match.py
@@ -1,8 +1,7 @@
 # Copyright (c) Microsoft Corporation. 
 # Licensed under the MIT license.
-
-from parser import DFG_python,DFG_java,DFG_ruby,DFG_go,DFG_php,DFG_javascript,DFG_csharp
-from parser import (remove_comments_and_docstrings,
+from parser2 import DFG_python,DFG_java,DFG_ruby,DFG_go,DFG_php,DFG_javascript,DFG_csharp
+from parser2 import (remove_comments_and_docstrings,
                    tree_to_token_index,
                    index_to_code_token,
                    tree_to_variable_index)
