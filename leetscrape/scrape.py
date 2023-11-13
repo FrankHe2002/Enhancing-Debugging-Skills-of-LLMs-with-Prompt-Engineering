b255d27cc8d3d891bcafd2abb1258030fbe4539d
MajikalExplosions <jliu7350@usc.edu>
2023-11-12 18:34:08 -0800

    Prepare for question scraping

---
+++ leetscrape/scrape.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from leetscrape.utils import get_all_questions_body
+from leetscrape.GetQuestionsList import GetQuestionsList
+
+ls = GetQuestionsList()
+ls.scrape() # Scrape the list of questions
+ls.to_csv(directory_path="./data/") # Save the scraped tables to a directory
+
+# This table can be generated using the previous commnd
+questions_info = pd.read_csv("./data/questions.csv")
+
+
+print("Getting question bodies...")
+# Scrape question body
+questions_body_list = get_all_questions_body(
+    questions_info["titleSlug"].tolist(),
+    questions_info["paidOnly"].tolist(),
+    save_to="./data/questionBody.pickle",
+)
+
+# Save to a pandas dataframe
+questions_body = pd.DataFrame(
+    questions_body_list
+).drop(columns=["titleSlug"])
+questions_body["QID"] = questions_body["QID"].astype(int)
+print(questions_body)
