25f327f4ac5ffa4862055d72a08026d158a07fee
Keyu(Frank) He <frankhe@usc.edu>
2023-11-18 19:48:53 -0800

    fix

---
+++ data/formatted/debugged_codes_0_n/Equal Row and Column Pairs_value.java
@@ -0,0 +1,25 @@
+```java
+class Solution {
+    public int equalPairs(int[][] grid) {
+        HashMap<String, Integer> map = new HashMap<>();
+        int row = grid.length;
+        int col = grid[0].length; // fix: use grid[0].length instead of grid.length
+        for (int i = 0; i < row; i++) {
+            String res = "";
+            for (int j = 0; j < col; j++) {
+                res += "-" + grid[i][j];
+            }
+            map.put(res, map.getOrDefault(res, 0) + 1);
+        }
+        int cnt = 0;
+        for (int j = 0; j < col; j++) {
+            String res = "";
+            for (int i = 0; i < row; i++) {
+                res += "-" + grid[i][j];
+            }
+            cnt += map.getOrDefault(res, 0);
+        }
+        return cnt;
+    }
+}
+```
